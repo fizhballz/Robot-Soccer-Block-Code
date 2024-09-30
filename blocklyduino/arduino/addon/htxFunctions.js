dc64c4912431a37721cd1cde952e13bce6ad7170
fizhballz <fizhballz@users.noreply.github.com>
2024-10-01 00:54:30 +0800

    uploading files

---
+++ blocklyduino/arduino/addon/htxFunctions.js
@@ -0,0 +1,87 @@
+/**
+ * @fileoverview Generating Arduino code for htxFunctions blocks.
+ */
+
+'use strict';
+
+goog.require('Blockly.Arduino');
+
+// Blockly.Arduino['goalangle'] = function(block) {
+//     // Get the value of the 'goal_angle' input field
+//     var angleValue = Blockly.Arduino.valueToCode(block, 'goal_angle', Blockly.Arduino.ORDER_NONE) || '0'; // Default to 0 if no angle is provided
+
+//     // Generate the Arduino code to set the goal angle
+//     var code = 'setGoalAngle(' + angleValue + ');\n';
+
+//     // Return the complete Arduino code with the appropriate formatting
+//     return code;
+// };
+Blockly.Arduino['readBall'] = function(block) {
+    // Get the value of the 'ball_sensor_number' input field
+    var ballSensorNumber = Blockly.Arduino.valueToCode(block, 'ball_sensor_number', Blockly.Arduino.ORDER_ATOMIC) || '1'; // Default to 1 if no sensor number is provided
+
+    // Generate the Arduino code for calling the readBall function
+    var code = 'readBall(' + ballSensorNumber + ')';
+    
+    // Return the complete Arduino code
+    return [code, Blockly.Arduino.ORDER_NONE]; // Return as an expression (for using the function in other expressions)
+};
+
+Blockly.Arduino['readCompass'] = function(block) {
+    // Generate the Arduino code for calling the readCompass function
+    var code = 'readCompass()';
+
+    // Return the complete Arduino code, returning it as part of an expression
+    return [code, Blockly.Arduino.ORDER_ATOMIC]; // Return as an expression (since this block outputs a value)
+};
+
+Blockly.Arduino['readButton'] = function(block) {
+    // Get the button number (1 or 2)
+    var buttonNumber = Blockly.Arduino.valueToCode(block, 'button_number', Blockly.Arduino.ORDER_ATOMIC) || '1';
+
+    // Generate the Arduino code for calling the readButton function
+    var code = 'readButton(' + buttonNumber + ')';
+
+    // Return the complete Arduino code, returning it as part of an expression
+    return [code, Blockly.Arduino.ORDER_ATOMIC]; // Return as an expression (since this block outputs a value)
+};
+
+Blockly.Arduino['readLine'] = function(block) {
+    // Get the line number (1, 2, or 3)
+    var lineNumber = Blockly.Arduino.valueToCode(block, 'line_number', Blockly.Arduino.ORDER_ATOMIC) || '1';
+
+    // Generate the Arduino code for calling the readLine function
+    var code = 'readLine(' + lineNumber + ')';
+
+    // Return the complete Arduino code, returning it as part of an expression
+    return [code, Blockly.Arduino.ORDER_ATOMIC]; // Return as an expression (since this block outputs a value)
+};
+Blockly.Arduino['motor'] = function(block) {
+    // Get the selected motor number (1, 2, or 3)
+    var motorNum = block.getFieldValue('motor_num').replace('motor', ''); // 'motor1' -> '1'
+    
+    // Get the speed and direction from the block inputs
+    var speed = block.getFieldValue('speed') || '0'; // Default speed is 0
+    var direction = block.getFieldValue('degree') || '0'; // Direction can be 0 or 1
+
+    // Generate the Arduino code
+    var code = 'controlMotor(' + motorNum + ', ' + speed + ', ' + direction + ');\n';
+    
+    // Return the code for motor control
+    return code;
+};
+Blockly.Arduino['loop'] = function(block) {
+    var loopCode = Blockly.Arduino.statementToCode(block, 'LOOP_CODE');
+    
+    // Generate the Arduino loop() function
+    var code = 'void loop() {\n' + loopCode + '}\n';
+    return code;
+};
+
+Blockly.Arduino['setup'] = function(block) {
+    var setupCode = Blockly.Arduino.statementToCode(block, 'SETUP_CODE');
+    
+    // Generate the Arduino setup() function
+    var code = 'void setup() {\n' + setupCode + '}\n';
+    return code;
+};
---
+++ blocklyduino/blocks/htxFunctions.js
@@ -0,0 +1,115 @@
+'use strict';
+
+goog.require('Blockly.Blocks');
+goog.require('Blockly');
+
+// Blockly.Blocks['goalangle'] = {
+//     init: function() {
+//         this.appendDummyInput()
+//             .setAlign(Blockly.ALIGN_LEFT)
+//             .appendField("Set goal angle")
+//             .appendField("angle:")
+//             .appendField(new Blockly.FieldNumber(0, -180, 180), 'goal_angle');
+//         this.setPreviousStatement(true, null);
+//         this.setNextStatement(true, null);
+//         this.setColour(235);
+//         this.setStyle('goalangle_blocks');
+//         this.setTooltip("Sets the goal angle");
+//         this.setHelpUrl("");
+//     }
+// };
+
+Blockly.Blocks ['readBall'] = {
+    init: function() {
+        this.appendDummyInput()
+            .appendField("Read Ball Sensor")
+            .appendField(new Blockly.FieldNumber(1, 1, 8), 'ball_sensor_number'); // Input field for the sensor number
+        this.setOutput(true, 'Number'); // Block returns a number (the sensor reading)
+        this.setColour(160);
+        this.setTooltip('Reads the value from the specified ball sensor (1-8)');
+        this.setHelpUrl('');
+    }
+};
+
+Blockly.Blocks['readCompass'] = {
+    init: function() {
+        this.appendDummyInput('')
+        .setAlign(Blockly.ALIGN_RIGHT)
+        .appendField('Compass Reading');
+        this.setOutput(true, 'Number'); // Outputs a number (double)
+        this.setTooltip('Reads the compass heading if calibrated');
+        this.setHelpUrl('');
+        this.setColour(160); // Set the color to something you prefer
+    }
+};  
+
+Blockly.Blocks['readButton'] = {
+    init: function() {
+        this.appendDummyInput()
+            .setAlign(Blockly.ALIGN_RIGHT)
+            .appendField("Read Button")
+            .appendField(new Blockly.FieldNumber(1, 1, 2), 'button_number'); // Number input limited to 1 or 2
+        this.setOutput(true, 'Number'); // Outputs a number (int)
+        this.setTooltip('Reads the state of the specified button (1 or 2)');
+        this.setHelpUrl('');
+        this.setColour(160); // Set block color
+    }
+};
+
+
+Blockly.Blocks['readLine'] = {
+    init: function() {
+        this.appendDummyInput()
+            .setAlign(Blockly.ALIGN_RIGHT)
+            .appendField("Read Line Sensor")
+            .appendField(new Blockly.FieldNumber(1, 1, 3), 'line_number'); // Number input limited to 1, 2, or 3
+        this.setOutput(true, 'Number'); // Outputs a number (int/analog value)
+        this.setTooltip('Reads the value from the specified line sensor (1, 2, or 3)');
+        this.setHelpUrl('');
+        this.setColour(225); // Set block color
+    }
+};
+
+//functions
+Blockly.Blocks['motor'] = {
+    init: function() {
+        this.appendDummyInput('')
+        .appendField(new Blockly.FieldDropdown([
+            ['motor 1', 'motor1'],
+            ['motor 2', 'motor2'],
+            ['motor 3', 'motor3']
+        ]), 'motor_num')
+        .appendField(new Blockly.FieldLabelSerializable('speed'), 'motor_speed')
+        .appendField(new Blockly.FieldNumber(0, 0), 'speed')
+        .appendField(new Blockly.FieldLabelSerializable('direction'), 'motor_dir')
+        .appendField(new Blockly.FieldNumber(0, 0, Infinity, 1), 'degree');
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setTooltip('');
+        this.setHelpUrl('');
+        this.setColour(285);
+    }
+};
+
+
+Blockly.Blocks ['void_loop'] = {
+    init: function() {
+        this.appendStatementInput('l')
+        .appendField('run forever');
+        this.setTooltip('');
+        this.setHelpUrl('');
+        this.setColour(120);
+    }
+};
+
+Blockly.Blocks['void_setup'] = {
+    init: function() {
+        this.appendStatementInput('l')
+        .appendField('run once');
+        this.setTooltip('');
+        this.setHelpUrl('');
+        this.setColour(120);
+    }
+};
+
+                      
